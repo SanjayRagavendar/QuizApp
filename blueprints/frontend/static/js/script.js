1fa85b67f160e3acc6aac5b626c42a6c3771b36d
JoyceBeulah <joycebeulah7@gmail.com>
2024-12-18 13:17:21 +0530

    updated frontend

---
+++ blueprints/frontend/static/js/script.js
@@ -0,0 +1,8 @@
+document.querySelector("form").addEventListener("submit", function(e) {
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+    if (!username || !password) {
+        alert("Please fill out all fields.");
+        e.preventDefault();
+    }
+});
